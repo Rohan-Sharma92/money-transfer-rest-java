a50f3fbc91ea0b0e7c29213b9a2b75603ddd429d
Rohan-Sharma92 <ronsharma1992@gmail.com>
2025-07-29 00:02:17 +0200

    Refactoring

---
+++ src/test/java/com/rohansharma/money_transfer_rest_java/MoneyTransferRestJavaApplicationTests.java
@@ -0,0 +1,11 @@
+package com.rohansharma.money_transfer_rest_java;
+
+import org.junit.jupiter.api.Test;
+
+class MoneyTransferRestJavaApplicationTests extends BaseIntegrationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
